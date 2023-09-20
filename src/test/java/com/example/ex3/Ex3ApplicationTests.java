bf006661567cbb8e9faf4fa9368f5d5dbf4dff02
Hong5743 <ghd5743@gmail.com>
2023-09-20 10:22:37 +0900

    샘플 프로젝트 시작

--- src/test/java/com/example/ex3/Ex3ApplicationTests.java
+++ src/test/java/com/example/ex3/Ex3ApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.ex3;
 
+import com.example.ex3.web.dto.SampleDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class Ex3ApplicationTests {
 
     @Test
     void contextLoads() {
+        SampleDto dto = SampleDto.builder()
+                .first("First1")
+                .last("Last1")
+                .regTime(LocalDateTime.now())
+                .sno(1L)
+                .build();
+        System.out.println(dto);
+        SampleDto dto2 = dto.toBuilder().sno(2L)
+                .regTime(LocalDateTime.now()).build();
+        System.out.println(dto2);
     }
 
 }
